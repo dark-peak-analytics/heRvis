15444b8e2c615e1959360e965e09817931cc7104
Robert Smith <rasmith3@sheffield.ac.uk>
2020-11-05 12:51:48 +0000

    hmm still can't fix

--- UI_parts/showDataModal.R
+++ UI_parts/showDataModal.R
@@ -1,2 +1,18 @@
 showDataModal <- modalDialog(title = "Show Data",
-                             fluidRow("Can't get this to work, want to insert first and last rows here"))
+                             fluidRow("Can't get this to work, want to insert first and last rows here, when I include the code tableOutput('validate_q1') this doesn't work... "),
+                             fluidRow(
+                               column(
+                                 offset = 1,
+                                 width = 10,
+                                 align = "center",
+                                 h4("Check the first and last 5 rows - looks ok?")
+                               ),
+                               br(),
+                               column(
+                                 offset = 1,
+                                 width = 10,
+                                 align = "center",
+                                 panel(tableOutput("validate_q1"))
+                               )
+                             )
+                             )

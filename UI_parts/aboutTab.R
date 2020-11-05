ec427a4c594a7a0857b6153b8e10fcc0878704db
Robert Smith <rasmith3@sheffield.ac.uk>
2020-11-05 10:51:05 +0000

    included footer and instructions

--- UI_parts/aboutTab.R
+++ UI_parts/aboutTab.R
@@ -1,3 +1,66 @@
-aboutTab <- tabPanel("About"
-         
+aboutTab <- tabPanel(
+  title = "About",
+  # Row1
+  fluidRow(column(
+    offset = 1,
+    width = 5,
+    h3("Step 1: Input Data"),
+    tags$ol(
+      tags$li("Input the names of the treatments"), 
+      tags$li("Paste in total costs & total QALYs"), 
+      tags$li("Check the first 5 and last 5 rows")
+    )
+  ),column(width = 5,
+           offset = 1,
+           align="center",
+           br(),
+    img(
+      src = "RotatingGif.gif",
+      align = "center",
+      height = '70px',
+      width = '70px'
+    ),
+    br()
+    )),
+  tags$hr(),
+  
+  
+  fluidRow(column(
+    offset = 1,
+    width = 5,
+    h3("Step 2: Customise & download"),
+    tags$ol(
+      tags$li("Select a plot or table"),
+      tags$li("Choose treatments and customise aesthetics"),
+      tags$li("Download the visual in the required format")
+      ),
+  ),column(width = 5,
+           offset = 1,
+           align="center",
+           br(),
+           img(
+             src = "RotatingGif.gif",
+             align = "center",
+             height = '70px',
+             width = '70px'
+           ),
+           br())),
+  tags$hr(),
+  
+  
+  fluidRow(column(
+    offset = 1,
+    width = 5,
+    h3("Step 3: Get a coffee")
+    ),column(width = 5,
+           offset = 1,
+           align="center",
+           img(
+             src = "RotatingGif.gif",
+             align = "center",
+             height = '70px',
+             width = '70px'
+           )))
+  
+  
 ) # end tab panel
--- UI_parts/footer.R
+++ UI_parts/footer.R
@@ -3,10 +3,12 @@ footer = div(
   class="footer",
   style="line-height: 20px;",
   br(),
-  column(1,align="right",
-         HTML("<img src='logo_concise.png' width='87px' height='100px'/>")
+  column(1,
+         align="right",
+         HTML("<img src='logo_concise.png' width='70px' height='70px'/>")
   ),
-  column(offset=1,8,
+  column(offset=1,
+         width = 8,
          br(),
          HTML("<p><b>heRvis</b> was created by Robert Smith & Paul Schneider of"),
          tags$a(href="https://www.darkpeakanalytics.com/", "Dark Peak Analytics."),
